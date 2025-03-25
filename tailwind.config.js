d1cf1debd5b6298dec900ccf6dd373c258594793
Kris MHC <krisxzzz@gmail.com>
2025-03-25 17:43:20 +0200

    update tailwinds config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 // tailwind.config.js
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./components/*.{js,ts,jsx,tsx}",
     "../shared_components/src/**/*.{js,ts,jsx,tsx}",
     // or if installed via npm (in node_modules)
     "./node_modules/shared_components/dist/**/*.{js,ts,jsx,tsx}",
