e5865cca1881d54d519d125f76a9645f670f16df
dogzy123 <i.gurskis.dev@gmail.com>
2025-03-25 17:50:41 +0200

    build tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
 // tailwind.config.js
 module.exports = {
   content: [
-    "./components/*.{js,ts,jsx,tsx}",
-    "../shared_components/src/*.{js,ts,jsx,tsx}",
-    // or if installed via npm (in node_modules)
-    "./node_modules/shared_components/dist/*.{js,ts,jsx,tsx}",
+      "./src/components/*.{html,js,tsx,jsx}"
   ],
   theme: {
     extend: {},
   },
   plugins: [],
+  corePlugins: {
+    preflight: false,
+  },
 };
