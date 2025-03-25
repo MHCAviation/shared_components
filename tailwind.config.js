5e8c8b7c8563215d1efb27b5f0497c51044545ca
Kris MHC <krisxzzz@gmail.com>
2025-03-25 17:47:47 +0200

    adjust folder path

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,9 @@
 module.exports = {
   content: [
     "./components/*.{js,ts,jsx,tsx}",
-    "../shared_components/src/**/*.{js,ts,jsx,tsx}",
+    "../shared_components/src/*.{js,ts,jsx,tsx}",
     // or if installed via npm (in node_modules)
-    "./node_modules/shared_components/dist/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/shared_components/dist/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
